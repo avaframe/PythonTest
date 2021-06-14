64e29c15825def1f7103f195973cd2fa1fe39aa8
Felix Oesterle <felix@avaframe.org>
2021-06-14 22:22:43 +0200

    now working with avaframe cython code... WTF....

--- myMod/myModSub/myExSub.py
+++ myMod/myModSub/myExSub.py
@@ -1,7 +1,10 @@
 # Python Module example
 import myMod.myModSub.myCython as mC
+import myMod.myModSub.dfaCy as dC
 
 def myMain():
    print('in myExSub')
    mC.myCy()
+   dC.mCy()
+   dC.pointsToRasterC()
 
--- myMod/myModSub/setup.py
+++ myMod/myModSub/setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
-    ext_modules = cythonize("myMod/myModSub/myCython.pyx")
+    # ext_modules = cythonize("myMod/myModSub/myCython.pyx"),
+    ext_modules = cythonize("myMod/myModSub/dfaCy.pyx",
+                          language_level=3),
+    include_dirs=[numpy.get_include(), '.']
 )
+
+
+# setup(
+#     ext_modules=cythonize("avaframe/com1DFA/DFAfunctionsCython.pyx",
+#                           compiler_directives={'linetrace': True},
+#                           language_level=3),
+# )
